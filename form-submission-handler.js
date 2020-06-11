e20303e511f075357c40ce5bb143c32deeef6f8f
Ricardo Chitagu <ricardo.everest@yahoo.com>
2020-06-11 06:14:21 +0200

    button disable

--- form-submission-handler.js
+++ form-submission-handler.js
@@ -1,21 +1,13 @@
-
-
-
 $('#form').submit(function(e) {
   e.preventDefault();
+  
   $.ajax({
       url: "https://script.google.com/macros/s/AKfycbxPZdJV4xFFBExS5vVmfyWsvhlWKu7C4ra5dAg-1w/exec",
       data: $(this).serialize(),
       type: "POST",
       dataType: "xml",
       success: function(data) {
-        Swal.fire({
-          position: 'center',
-          icon: 'success',
-          title: 'Thanks for contacting us. We will get back to you shortly',
-          showConfirmButton: false,
-          timer: 3000
-     })
+     
       },
       error: function(xhr, status, error) {
           console.log('Submission failed: ' + error);
@@ -29,10 +21,13 @@ function validateForm(){
     icon: 'success',
     title: 'Thanks for contacting us. We will get back to you shortly',
     showConfirmButton: false,
-    timer: 3000
+    timer: 4000
 })
+$( "#button" ).prop( "disabled", true );
+$( "#button" ).css( {"background-color":"red"},{"border":"0px"});
 }
-    
+
+
 
     
 
